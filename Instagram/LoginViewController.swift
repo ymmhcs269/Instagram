20afe87e8bedb381139b2b15ee26455282eacaba
marino <ymmh.cs2016@gmail.com>
2016-08-18 11:48:50 +0900

    ビルド成功

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -23,25 +23,32 @@ class LoginViewController: UIViewController {
    
     
     // ログインボタンをタップしたときに呼ばれるメソッド
-    
     @IBAction func handleLoginButton(sender: AnyObject) {
-    
         if let address = mailAddressTextField.text, let password = passwordTextField.text {
             
-            // アドレスとパスワード名のいずれかでも入力されていない時は何もしない
+            // アドレスとパスワード名のいずれかでも入力されていない時はHUDを出して何もしない
             if address.characters.isEmpty || password.characters.isEmpty {
-                
+                SVProgressHUD.showErrorWithStatus("必要項目を入力して下さい")
                 return
             }
             
+            // 処理中を表示
+            SVProgressHUD.show()
+            
             FIRAuth.auth()?.signInWithEmail(address, password: password) { user, error in
                 if error != nil {
+                    // エラー表示
+                    SVProgressHUD.showErrorWithStatus("エラー")
+                    
                     print(error)
                 } else {
                     // Firebaseからログインしたユーザの表示名を取得してNSUserDefaultsに保存する
                     if let displayName = user?.displayName {
                         self.setDisplayName(displayName)
                     }
+                    
+                    // HUDを消す
+                    SVProgressHUD.dismiss()
                     
                     // 画面を閉じる
                     self.dismissViewControllerAnimated(true, completion: nil)
@@ -50,26 +57,31 @@ class LoginViewController: UIViewController {
         }
     }
     
+    
     // アカウント作成ボタンをタップしたときに呼ばれるメソッド
     
     @IBAction func handleCreateAcountButton(sender: AnyObject) {
-    
         if let address = mailAddressTextField.text, let password = passwordTextField.text,
             let displayName = displayNameTextField.text {
             // アドレスとパスワードと表示名のいずれかでも入力されていない時は何もしない
             if address.characters.isEmpty || password.characters.isEmpty
                 || displayName.characters.isEmpty {
-                
+                SVProgressHUD.showErrorWithStatus("必要項目を入力して下さい")
                 return
             }
             
+            // HUDで処理中を表示
+            SVProgressHUD.show()
+            
             FIRAuth.auth()?.createUserWithEmail(address, password: password) { user, error in
                 if error != nil {
+                    SVProgressHUD.showErrorWithStatus("エラー")
                     print(error)
                 } else {
                     // ユーザーを作成できたらそのままログインする
                     FIRAuth.auth()?.signInWithEmail(address, password: password) { user, error in
                         if error != nil {
+                            SVProgressHUD.showErrorWithStatus("エラー")
                             print(error)
                         } else {
                             if let user = user {
@@ -82,6 +94,9 @@ class LoginViewController: UIViewController {
                                     } else {
                                         // NSUserDefaultsに表示名を保存する
                                         self.setDisplayName(displayName)
+                                        
+                                        // HUDを消す
+                                        SVProgressHUD.dismiss()
                                         
                                         // 画面を閉じる
                                         self.dismissViewControllerAnimated(true, completion: nil)
